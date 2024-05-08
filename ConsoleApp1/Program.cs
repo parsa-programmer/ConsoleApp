bc9dd047d8de5d37dc05a3a45dea6e9523e51ad1
S07C05 <S07C05@S07C05>
2024-05-08 09:25:11 +0330

    Zarb

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,10 +9,17 @@ var secondNumber = 0;
 int.TryParse(a,out firstNumber);
 int.TryParse(b,out secondNumber);
 Sum(firstNumber, secondNumber);
+Zarb(firstNumber, secondNumber);
 
 
 
 
 void Sum(int one,int two){
+    Console.Write("Jam Adad : ");
     Console.WriteLine(one + two);
+}
+void Zarb(int one, int two)
+{
+    Console.Write("Zarbe Adad : ");
+    Console.WriteLine(one * two);
 }
